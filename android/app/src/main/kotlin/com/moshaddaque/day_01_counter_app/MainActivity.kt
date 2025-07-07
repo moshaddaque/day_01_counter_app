a83c009e93c1974cc25a8411ab012773467922df
moshaddaque <info.moshaddaque@gmail.com>
2025-07-07 10:35:01 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/moshaddaque/day_01_counter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.moshaddaque.day_01_counter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:day_01_counter_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'day_01_counter_app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
